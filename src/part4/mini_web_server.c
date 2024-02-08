967f91c8d410ada62fa333ed34a5a51a0d0bc3fa
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-08 16:37:50 -0500

    Delete src directory

--- src/part4/mini_web_server.c
+++
@@ -1,9 +0,0 @@
-#include <fcgi_stdio.h>
-
-int main(void) {
-    while (FCGI_Accept() >= 0) {
-        printf("Content-type: text/html\r\n\r\n");
-        printf("Hello World!");
-    }
-    return 0;
-}
--- src/part4/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
- 
-gcc -o mini_web_server mini_web_server.c -lfcgi
-spawn-fcgi -p 8080 ./mini_web_server
-service nginx start
-/bin/bash
